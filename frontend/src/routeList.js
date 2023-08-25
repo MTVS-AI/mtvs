975d8e0650f7735ab60f153a41e26d919325d01a
inchae <dlsco1230@gmail.com>
2023-08-25 17:34:57 +0900

    YOLO+OCR+GPT=="CSV"

--- frontend/src/routeList.js
+++ frontend/src/routeList.js
@@ -2,9 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Main from "./pages/Main";
-import Login from "./pages/Login";
-import Logout from "./pages/Logout";
-import Register from "./pages/Register";
 
 const Router = () => {
   return (
@@ -12,9 +9,6 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} exact />
         <Route path="/main" element={<Main />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
